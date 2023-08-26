e776a383cfe98a4379c65730291d7bc4d18ffebd
danieldev23 <huydevc23@gmail.com>
2023-08-26 16:41:17 +0700

    Thuật toán kiểm tra số nguyên tố 19/08/2023

--- PrimeAlgorithm.java
+++ PrimeAlgorithm.java
@@ -16,15 +16,13 @@ public class PrimeAlgorithm {
     return true;
   }
 
-  public static void main(String[] args) {
-    
+  public static void main(String[] args) { 
+	  
     Scanner sc = new Scanner(System.in);
-    
     System.out.println("Nhập một số: ");
     int n = sc.nextInt();
-
     boolean prime = isPrime(n);
-
+    
     if(prime) {
       System.out.println(n + " là số nguyên tố");
     }
